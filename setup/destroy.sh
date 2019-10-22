49fd2ad8b7c20fd0bb8fb7bb4da6d032fbabee5f
Ruby Noel <rubyfelicia.noel@gmail.com>
2019-10-22 14:16:50 +1100

    added cicd scripts

--- setup/destroy.sh
+++ setup/destroy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # deploy infra and network dependencies
-docker build . -t pwc-base-infra:latest --file ./network/Dockerfile
+docker build . -t pwc-base-destroy:latest --file ./network/Dockerfile
 
-STAGE=dev AWS_REGION=ap-southeast-2 docker run --env STAGE --env AWS_REGION pwc-base-infra:latest
+STAGE=dev AWS_REGION=ap-southeast-2 docker run --env STAGE --env AWS_REGION pwc-base-destroy:latest
