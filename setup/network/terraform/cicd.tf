287cf2d058273af925cd2be6234432b162956876
Ruby Noel <rubyfelicia.noel@gmail.com>
2019-10-22 15:09:59 +1100

    added cicd scripts

--- setup/network/terraform/cicd.tf
+++ setup/network/terraform/cicd.tf
@@ -81,7 +81,7 @@ resource "aws_codepipeline_webhook" "codepipeline_webhook" {
 
   filter {
     json_path    = "$.ref"
-    match_equals = "refs/heads/{Branch}/setup"
+    match_equals = "refs/heads/{Branch}"
   }
 }
 
