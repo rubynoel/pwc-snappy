1246ab4b223a33a08dda2e21e0554401573c97de
Ruby Noel <rubyfelicia.noel@gmail.com>
2019-10-23 15:12:26 +1100

    codebuild

--- setup/cicd/terraform/codebuild.tf
+++ setup/cicd/terraform/codebuild.tf
@@ -122,6 +122,7 @@ resource "aws_codebuild_project" "codebuild_project" {
     image                       = "aws/codebuild/standard:1.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
+    privileged_mode = true
     
     environment_variable {
       name  = "STAGE"
