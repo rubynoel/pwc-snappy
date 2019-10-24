9e3a8bf994461b6b9e44fa825398e5a009616be7
Ruby Noel <rubyfelicia.noel@gmail.com>
2019-10-24 19:21:53 +1100

    added role

--- sync-company-status-batch/src/entrypoint.sh
+++ sync-company-status-batch/src/entrypoint.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
+#TODO: better error handling
 exit_on_error() {
     if [ $? -eq 0 ]; then
         echo "Error in job. Exiting."
+        exit 1
     fi
 }
 
 get_ssm_parameter() {
-    local value = $(aws ssm get-parameter --name "$1" --with-decryption | jq --raw-output '.Parameter.Value')
-    if [ -z "$value" ]; then
-        echo "Unable to get SSM Parameter $1"
-        exit 1
-    else
-        echo "$1 is $value"
-    fi
-    return value
+    return $(aws ssm get-parameter --name "$1" --with-decryption | jq --raw-output '.Parameter.Value')
 }
 echo "Starting the job shell script"
 
 pwd && ls
+
+# set default value for env variables if not set
+echo RESOURCE_REGION is $RESOURCE_REGION
+export RESOURCE_REGION="${RESOURCE_REGION:-ap-southeast-2}"
+export AWS_DEFAULT_REGION=$RESOURCE_REGION
 
 export DB_HOST=$(get_ssm_parameter "$SSM_KEY_DB_ENDPOINT")
 export DB_USERNAME=$(get_ssm_parameter "$SSM_KEY_DB_USER")
