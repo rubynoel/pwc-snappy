7787745e13f775dd878c078aebbb180d978d391c
Ruby Noel <rubyfelicia.noel@gmail.com>
2019-10-24 20:04:18 +1100

    added role

--- sync-company-status-batch/src/entrypoint.sh
+++ sync-company-status-batch/src/entrypoint.sh
@@ -9,7 +9,7 @@ exit_on_error() {
 }
 
 get_ssm_parameter() {
-    return $(aws ssm get-parameter --name "$1" --with-decryption | jq --raw-output '.Parameter.Value')
+    return $(aws ssm get-parameter --name $1 --with-decryption | jq --raw-output '.Parameter.Value')
 }
 echo "Starting the job shell script"
 
@@ -20,11 +20,11 @@ echo RESOURCE_REGION is $RESOURCE_REGION
 export RESOURCE_REGION="${RESOURCE_REGION:-ap-southeast-2}"
 export AWS_DEFAULT_REGION=$RESOURCE_REGION
 
-export DB_HOST=$(get_ssm_parameter "$SSM_KEY_DB_ENDPOINT")
-export DB_USERNAME=$(get_ssm_parameter "$SSM_KEY_DB_USER")
-export DB_PASSWORD=$(get_ssm_parameter "$SSM_KEY_DB_PASSWORD")
-export DB_NAME=$(get_ssm_parameter "$SSM_KEY_DB_NAME")
-export DB_PORT=$(get_ssm_parameter "$SSM_KEY_DB_PORT")
+$(get_ssm_parameter "$SSM_KEY_DB_ENDPOINT") && export DB_HOST=$?
+$(get_ssm_parameter "$SSM_KEY_DB_USER") && export DB_USER=$?
+$(get_ssm_parameter "$SSM_KEY_DB_PASSWORD") && export DB_PASSWORD=$?
+$(get_ssm_parameter "$SSM_KEY_DB_NAME") && export DB_NAME=$?
+$(get_ssm_parameter "$SSM_KEY_DB_PORT") && export DB_PORT=$?
 
 #node node_modules/db-migrate/bin/db-migrate
 node index.js
