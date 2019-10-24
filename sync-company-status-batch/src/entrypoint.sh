7cbea2b9c045e50e61c52e67f783c7b79fb6c645
Ruby Noel <rubyfelicia.noel@gmail.com>
2019-10-24 14:43:02 +1100

    added rds

--- sync-company-status-batch/src/entrypoint.sh
+++ sync-company-status-batch/src/entrypoint.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
-export DB_USERNAME=$(aws ssm get-parameter --name "$SSM_KEY_DB_USER" --with-decryption | jq --raw-output '.Parameter.Value')
-echo DB_USERNAME is $DB_USERNAME
+exit_on_error() {
+    if [ $? -eq 0 ]; then
+        echo "Error in job. Exiting."
+    fi
+}
+
+get_ssm_parameter() {
+    local value = $(aws ssm get-parameter --name "$1" --with-decryption | jq --raw-output '.Parameter.Value')
+    if [ -z "$value" ]; then
+        echo "Unable to get SSM Parameter $1"
+        exit 1
+    else
+        echo "$1 is $value"
+    fi
+    return value
+}
+echo "Starting the job shell script"
+
+pwd && ls
+
+export DB_HOST=$(get_ssm_parameter "$SSM_KEY_DB_ENDPOINT")
+export DB_USERNAME=$(get_ssm_parameter "$SSM_KEY_DB_USER")
+export DB_PASSWORD=$(get_ssm_parameter "$SSM_KEY_DB_PASSWORD")
+export DB_NAME=$(get_ssm_parameter "$SSM_KEY_DB_NAME")
+export DB_PORT=$(get_ssm_parameter "$SSM_KEY_DB_PORT")
 
 node index.js
