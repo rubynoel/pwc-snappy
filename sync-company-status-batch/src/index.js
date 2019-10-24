a5734a03ee3d3bdf1405845d1bc5a14084fa7080
Ruby Noel <rubyfelicia.noel@gmail.com>
2019-10-24 12:38:24 +1100

    added rds

--- sync-company-status-batch/src/index.js
+++ sync-company-status-batch/src/index.js
@@ -1,39 +1,38 @@
-"use strict";
+'use strict';
 
-const AWS = require("aws-sdk");
-const region = process.env["RESOURCE_REGION"];
-const jobProcessor = require("./sync-company-status");
+const AWS = require('aws-sdk');
+const jobProcessor = require('./sync-company-status');
 const ssm = new AWS.SSM();
 
 AWS.config.update({
-  region: "region"
+  region: process.env['RESOURCE_REGION'],
 });
 
 const processJob = () => {
-  console.log("Running batch..");
+  console.log('Running batch..');
 
   jobProcessor.syncCompanyStatus(
-    (dbConfig = {
-      user: getSSMParameter(process.env["SSM_KEY_DB_ENDPOINT"]),
-      password: getSSMParameter(process.env["SSM_KEY_DB_ENDPOINT"]),
-      database: getSSMParameter(process.env["SSM_KEY_DB_ENDPOINT"]),
-      host: getSSMParameter(process.env["SSM_KEY_DB_ENDPOINT"]),
-      port: getSSMParameter(process.env["SSM_KEY_DB_PORT"])
-    })
+      (dbConfig = {
+        user: getSSMParameter(process.env['SSM_KEY_DB_ENDPOINT']),
+        password: getSSMParameter(process.env['SSM_KEY_DB_ENDPOINT']),
+        database: getSSMParameter(process.env['SSM_KEY_DB_ENDPOINT']),
+        host: getSSMParameter(process.env['SSM_KEY_DB_ENDPOINT']),
+        port: getSSMParameter(process.env['SSM_KEY_DB_PORT']),
+      })
   );
 };
 
-const getSSMParameter = name => {
+const getSSMParameter = (name) => {
   ssm
-    .getParameter({
-      Name: `${name}`,
-      WithDecryption: true
-    })
-    .promise()
-    .then(data =>
-      data.Parameter
-        ? data.Parameter.Value
-        : Promise.reject(new Error(`SSM Parameter ${name} is not set.`))
-    );
+      .getParameter({
+        Name: `${name}`,
+        WithDecryption: true,
+      })
+      .promise()
+      .then((data) =>
+      data.Parameter ?
+        data.Parameter.Value :
+        Promise.reject(new Error(`SSM Parameter ${name} is not set.`))
+      );
 };
 processJob();
