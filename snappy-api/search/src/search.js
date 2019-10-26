dc31280b8201829e99e4406dbb717995a971836e
Ruby Noel <rubyfelicia.noel@gmail.com>
2019-10-26 17:30:32 +1100

    eslint ignore dist

--- snappy-api/search/src/search.js
+++ snappy-api/search/src/search.js
@@ -9,11 +9,15 @@ const searchFields = {
   RESTRICTED_STATUS: 'restrictedStatus',
 };
 
-const pageLimit = 100;
+const pageLimitMax = 100;
 
 const search = async (params) => {
   const pgPool = params.isMocked ? params.poolMock : getPool();
 
+  const pageLimit =
+    params.limit && params.limit > 0 && params.limit <= pageLimitMax ?
+      params.limit :
+      pageLimitMax;
   switch (params.fieldName) {
     case searchFields.COMPANY_NAME:
       const searchResults = await searchByCompanyName(pgPool, {
@@ -21,6 +25,7 @@ const search = async (params) => {
         offset: params.from,
         limit: pageLimit,
       });
+      console.log(`${searchResults}`);
       break;
     case searchFields.BUSINESS_NUMER:
       break;
